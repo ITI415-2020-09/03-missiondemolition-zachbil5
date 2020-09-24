affcf51cdf630edf086f3f923af220d11b783d46
zachbil5 <55146215+zachbil5@users.noreply.github.com>
2020-09-24 17:39:18 -0400

    V1
    All base objects have been created

---
+++ Assets/Scripts/Slingshot.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Slingshot : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void OnMouseEnter()
+    {
+        print("Slingshot:OnMouseEnter()");
+    }
+    void OnMouseExit()
+    {
+        print("Slingshot:OnMouseExit()");
+    }
+}
