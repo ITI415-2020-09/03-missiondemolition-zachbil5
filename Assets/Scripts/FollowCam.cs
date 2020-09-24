b2a503321db207cdd724c1b0923421613c2e5f54
zachbil5 <55146215+zachbil5@users.noreply.github.com>
2020-09-24 18:33:25 -0400

    v2

---
+++ Assets/Scripts/FollowCam.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCam : MonoBehaviour
+{
+    // Start is called before the first frame update
+    static public FollowCam S; // a FollowCam Singleton
+                               // fields set in the Unity Inspector pane
+    public bool _____________________________;
+    // fields set dynamically
+    public GameObject poi; // The point of interest
+    public float camZ; // The desired Z pos of the camera
+    public float easing = 0.05f;
+    public Vector2 minXY;
+    void Awake()
+    {
+        S = this;
+        camZ = this.transform.position.z;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        // if there's only one line following an if, it doesn't need braces
+        if (poi == null) return; // return if there is no poi
+                                 // Get the position of the poi
+        Vector3 destination = poi.transform.position;
+        destination.x = Mathf.Max(minXY.x, destination.x);
+        destination.y = Mathf.Max(minXY.y, destination.y);
+        destination = Vector3.Lerp(transform.position, destination, easing);
+        // Retain a destination.z of camZ
+        destination.z = camZ;
+        // Set the camera to the destination
+        transform.position = destination;
+        this.GetComponent<Camera>().orthographicSize = destination.y + 10;
+    }
+}
